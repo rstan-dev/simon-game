df9b6d1354032e8472419ac3545989022b31547a
Russ Smith <rstanleym74@gmail.com>
2023-05-22 17:07:08 +0100

    add turnInprogress and lastButtons tests and functions

--- assets/scripts/tests/game.test.js
+++ assets/scripts/tests/game.test.js
@@ -34,6 +34,12 @@ describe("game object contains correct keys", () => {
     test("turnNumber key exists", () => {
         expect("turnNumber" in game).toBe(true);
     });
+    test("lastButton key exists", () => {
+        expect("lastButton" in game).toBe(true);
+    });
+    test("turnInprogress key exists", () => {
+        expect("turnInProgress" in game).toBe(true);
+    });
 
 });
 
@@ -104,6 +110,16 @@ describe("game play works correctly", () => {
         game.playerMoves.push("wrong");
         playerTurn();
         expect(window.alert).toBeCalledWith("Wrong move!");
+    });
+    test("should toggle turnInProgress to true", () => {
+        showTurns();
+        expect(game.turnInProgress).toBe(true);
+    });
+    test("clicking during computer sequence should fail", () => {
+        showTurns();
+        game.lastButton = "";
+        document.getElementById("button2").click();
+        expect(game.lastButton).toEqual("");
     });
 
 });
